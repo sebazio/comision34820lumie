7456ee38d3a6bc71898e37e6394c9d127d989acf
sebazdev <szuviria@gmail.com>
2022-11-14 22:33:33 -0300

    Clase de jsx

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <nav className='navbar'>
+            <h1 style={{ color: 'red'}}>MyApp</h1>
+            <button className='boton'>boton 1</button>
+            <button className='boton'>boton 2</button>
+        </nav>
+    )
+}
+
+export default Navbar
+
