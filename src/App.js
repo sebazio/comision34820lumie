947d000261976fc940809d732c88e5b34754d6d6
sebazdev <szuviria@gmail.com>
2022-11-16 22:38:13 -0300

    Clase de componentes 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Navbar colorTitle='red'/>
+      <ItemListContainer greeting='Saludo de bienvenida'/>
     </div>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,31 @@
 import './Navbar.css'
+import Button from '../Button/Button'
+import CartWidget from '../CartWidget/CartWidget'
 
-const Navbar = () => {
+const Navbar = (props) => {
+    console.log(props)
+
+    const handleOnClick = () => {
+        alert('hice click')
+    }
+    
+    const handleOnClick2 = () => {
+        alert('hice otro click')
+    }
+
+
     return (
         <nav className='navbar'>
-            <h1 style={{ color: 'red'}}>MyApp</h1>
-            <button className='boton'>boton 1</button>
-            <button className='boton'>boton 2</button>
+            <h1 style={{ color: props.colorTitle}}>MyApp</h1>
+            <Button colorText='red' func={handleOnClick}>Home</Button>
+            <Button colorText='blue' func={handleOnClick2}>About</Button>
+            <Button>
+                Contact
+            </Button>
+            <Button>
+                Login
+            </Button>
+            <CartWidget />
         </nav>
     )
 }
